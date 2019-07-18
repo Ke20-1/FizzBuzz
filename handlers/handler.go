060e4b941f0250791fa1cc8aa5e5c376fe1206d3
kevinb <kevin.bettan@treezor.com>
2019-07-18 14:56:26 +0200

    modif handler

--- handlers/handler.go
+++ handlers/handler.go
@@ -31,14 +31,23 @@ func FizzBuzzHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 		fb, err := parseParameters(r.URL.Query())
 		if err != nil {
-			w.Write([]byte(err.Error()))
+			_, err = w.Write([]byte(err.Error()))
+			if err != nil {
+				log.Errorf("error to send error response : %v", err)
+			}
 		}
 		buffer := &bytes.Buffer{}
 
-		gob.NewEncoder(buffer).Encode(algo.FizzBuzzAlgo(fb))
+		err = gob.NewEncoder(buffer).Encode(algo.FizzBuzzAlgo(fb))
+		if err != nil {
+			log.Errorf("error to encode response : %v", err)
+		}
 		log.Info(algo.FizzBuzzAlgo(fb))
 
-		w.Write(buffer.Bytes())
+		_, err = w.Write(buffer.Bytes())
+		if err != nil {
+			log.Errorf("error to sending response : %v", err)
+		}
 	} else {
 		log.Info("request discard because not a post")
 	}
@@ -53,23 +62,23 @@ func parseParameters(param url.Values) (*algo.FizzBuzz, error) {
 	limit, oklimit := param["limit"]
 
 	if !okfizz || len(fizz[0]) < 1 {
-		return nil, fmt.Errorf("Url Param 'key' is missing")
+		return nil, fmt.Errorf("url Param 'key' is missing")
 	}
 	if !okbuzz || len(buzz[0]) < 1 {
-		log.Println("Url Param 'key' is missing")
-		return nil, fmt.Errorf("Url Param 'key' is missing")
+		log.Println("url Param 'key' is missing")
+		return nil, fmt.Errorf("url Param 'key' is missing")
 	}
 	if !okval1 || len(val1[0]) < 1 {
-		log.Println("Url Param 'key' is missing")
-		return nil, fmt.Errorf("Url Param 'key' is missing")
+		log.Println("url Param 'key' is missing")
+		return nil, fmt.Errorf("url Param 'key' is missing")
 	}
 	if !okval2 || len(val2[0]) < 1 {
-		log.Println("Url Param 'key' is missing")
-		return nil, fmt.Errorf("Url Param 'key' is missing")
+		log.Println("url Param 'key' is missing")
+		return nil, fmt.Errorf("url Param 'key' is missing")
 	}
 	if !oklimit || len(limit[0]) < 1 {
-		log.Println("Url Param 'key' is missing")
-		return nil, fmt.Errorf("Url Param 'key' is missing")
+		log.Println("url Param 'key' is missing")
+		return nil, fmt.Errorf("url Param 'key' is missing")
 	}
 
 	fizzF := fizz[0]
@@ -78,20 +87,20 @@ func parseParameters(param url.Values) (*algo.FizzBuzz, error) {
 	val2F := val2[0]
 	limitF := limit[0]
 
-	log.Println("Url Param 'fizz' is: " + string(fizzF))
-	log.Println("Url Param 'buzzF' is: " + string(buzzF))
-	log.Println("Url Param 'val1F' is: " + string(val1F))
-	log.Println("Url Param 'val2F' is: " + string(val2F))
-	log.Println("Url Param 'limitF' is: " + string(limitF))
-	val1Num, err := strconv.Atoi(string(val1F))
+	log.Println("Url Param 'fizz' is: " + fizzF)
+	log.Println("Url Param 'buzzF' is: " + buzzF)
+	log.Println("Url Param 'val1F' is: " + val1F)
+	log.Println("Url Param 'val2F' is: " + val2F)
+	log.Println("Url Param 'limitF' is: " + limitF)
+	val1Num, err := strconv.Atoi(val1F)
 	if err != nil {
 		return nil, err
 	}
-	val2Num, err := strconv.Atoi(string(val2F))
+	val2Num, err := strconv.Atoi(val2F)
 	if err != nil {
 		return nil, err
 	}
-	limitNum, err := strconv.Atoi(string(limitF))
+	limitNum, err := strconv.Atoi(limitF)
 	if err != nil {
 		return nil, err
 	}
